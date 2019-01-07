241a570fb50d024dbee33bf509d894805945345b
yangcy <18796014744@163.com>
2019-01-07 14:52:34 +0800

    增加memcached测试学习代码

---
+++ src/main/java/com/yangcy/memcached/MemcachedAppend.java
@@ -0,0 +1,43 @@
+package com.yangcy.memcached;
+
+import java.net.InetSocketAddress;
+import java.util.concurrent.Future;
+ 
+import net.spy.memcached.MemcachedClient;
+ 
+public class MemcachedAppend{
+   public static void main(String[] args) {
+   
+      try{
+   
+         // 连接本地的 Memcached 服务
+         MemcachedClient mcc = new MemcachedClient(new InetSocketAddress("127.0.0.1", 11211));
+         System.out.println("Connection to server sucessful.");
+ 
+         // 添加数据
+         Future fo = mcc.set("runoob", 900, "Free Education");
+ 
+         // 输出执行 set 方法后的状态
+         System.out.println("set status:" + fo.get());
+ 
+         // 获取键对应的值
+         System.out.println("runoob value in cache - " + mcc.get("runoob"));
+         
+         // 对存在的key进行数据添加操作
+         
+         fo = mcc.append(mcc.gets("runoob").getCas(), "runoob", " for All");
+ 
+         // 输出执行 set 方法后的状态
+         System.out.println("append status:" + fo.get());
+         
+         // 获取键对应的值
+         System.out.println("runoob value in cache - " + mcc.get("runoob"));
+ 
+         // 关闭连接
+         mcc.shutdown();
+         
+      }catch(Exception ex) {
+         System.out.println(ex.getMessage());
+      }
+   }
+}
